d3544b4eab7b163be0d0b48b2b92989ccbf4618f
Richard Barbour <richyb07@gmail.com>
2023-10-27 13:11:46 -0700

    Add validation to Park.cs model.

--- ParksApi/Models/Park.cs
+++ ParksApi/Models/Park.cs
@@ -1,18 +1,40 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ParksApi.Models
 {
-  public class Park
+  public class Park : IValidatableObject
   {
     public int ParkId { get; set; }
 
+    [Required]
     public string Name { get; set; }
-
+    [Required]
     public string Location { get; set; }
-
+    [Required]
     public string Authority { get; set; }
 
     public readonly string[] availableLocations = { "AK", "CA" };
 
     public readonly string[] availableAuthorities = { "National", "State" };
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+      if (Array.IndexOf(availableLocations, Location) == -1)
+      {
+        yield return new ValidationResult(
+          "You must use two-letter USPS state abbreviations. Currently only Alaska ('AK') and California ('CA') supported."
+        );
+      }
+
+      if (Array.IndexOf(availableAuthorities, Authority) == -1)
+      {
+        yield return new ValidationResult(
+          "Only 'National' or 'State' accepted as an authority."
+        );
+      }
+    }
+
+
   }
   
 }
